da4b08afd65f59ddfa185e29c9c7ac757b0eb034
Mauricio Vazquez <maurovz@gmail.com>
2024-09-17 18:37:13 +0200

    feat: create constants and move factory to its own file

--- BikeStation/BikeStationApp.swift
+++ BikeStation/BikeStationApp.swift
@@ -9,24 +9,3 @@ struct BikeStationApp: App {
       }
   }
 }
-
-struct BikeStationListViewFactory {
-  static func create() -> BikeStationListView {
-    let viewModel = BikeStationsViewModel(loader: getLoader(), didSelectAction: openMapCoordinates)
-    return BikeStationListView(viewModel: viewModel)
-  }
-
-  static func getLoader() -> RemoteStationLoader {
-    let client = URLSessionHTTPClient(session: .shared)
-    return RemoteStationLoader(url: Constants.url, client: client, cache: CoreDataServices())
-  }
-}
-
-func openMapCoordinates(for viewModel: StationItemViewModel) {
-  let urlString = "http://maps.apple.com/?ll=\(viewModel.latitude),\(viewModel.longitude)"
-  UIApplication.shared.open(URL(string: urlString)!, options: [:], completionHandler: nil)
-}
-
-struct Constants {
-  static let url = URL(string: "https://api.citybik.es/v2/networks/wienmobil-rad")!
-}
--- BikeStation/Factories/BikeStationListViewFactory.swift
+++ BikeStation/Factories/BikeStationListViewFactory.swift
@@ -1,8 +1,19 @@
-//
-//  BikeStationListViewFactory.swift
-//  BikeStation
-//
-//  Created by Mauricio Vazquez on 17/9/24.
-//
+import UIKit
+import BikeDataFeature
 
-import Foundation
+struct BikeStationListViewFactory {
+  static func create() -> BikeStationListView {
+    let viewModel = BikeStationsViewModel(loader: getLoader(), didSelectAction: openMapCoordinates)
+    return BikeStationListView(viewModel: viewModel)
+  }
+
+  static func getLoader() -> RemoteStationLoader {
+    let client = URLSessionHTTPClient(session: .shared)
+    return RemoteStationLoader(url: Constants.url, client: client, cache: CoreDataServices())
+  }
+}
+
+func openMapCoordinates(for viewModel: StationItemViewModel) {
+  let urlString = "http://maps.apple.com/?ll=\(viewModel.latitude),\(viewModel.longitude)"
+  UIApplication.shared.open(URL(string: urlString)!, options: [:], completionHandler: nil)
+}
