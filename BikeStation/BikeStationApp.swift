d05ab8f094339c7654138267a70c7b50499fac9c
Mauricio Vazquez <maurovz@gmail.com>
2024-09-17 17:19:53 +0200

    Init

---
+++ BikeStation/BikeStationApp.swift
@@ -0,0 +1,32 @@
+//
+//  BikeStationApp.swift
+//  BikeStation
+//
+//  Created by Mauricio Vazquez on 17/9/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BikeStationApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
