927786d816cddd4f06682bf3ea516a0f7bd4034a
Mauricio Vazquez <maurovz@gmail.com>
2024-09-17 17:50:47 +0200

    feat: create coredata infra, model and service

--- BikeDataFeature/Station Feature/Networking/RemoteStationLoader.swift
+++ BikeDataFeature/Station Feature/Networking/RemoteStationLoader.swift
@@ -3,12 +3,12 @@ import Foundation
 public final class RemoteStationLoader: StationLoader {
   private let url: URL
   private let client: HTTPClient
-//  private let cache: CoreDataServices
+  private let cache: CoreDataServices
 
-  public init(url: URL, client: HTTPClient) { //, cache: CoreDataServices) {
+  public init(url: URL, client: HTTPClient, cache: CoreDataServices) {
     self.url = url
     self.client = client
-//    self.cache = cache
+    self.cache = cache
   }
 
   public typealias Result = StationLoader.Result
@@ -26,19 +26,19 @@ public final class RemoteStationLoader: StationLoader {
         completion(.success(mappedStation))
 
       case .failure(let error):
-//        let cacheStations = self.cache.loadStationsFromCoreData()
-//        guard !cacheStations.isEmpty else {
+        let cacheStations = self.cache.loadStationsFromCoreData()
+        guard !cacheStations.isEmpty else {
           completion(.failure(error))
           return
-//        }
-//
-//        completion(.success(cacheStations))
+        }
+
+        completion(.success(cacheStations))
       }
     }
   }
 
   private func setCache(stations: [Station]) {
-//    guard cache.deleteEntityFromCoreData(entity: "StationEntity") else { return }
-//    cache.saveStationsToCache(stations: stations)
+    guard cache.deleteEntityFromCoreData(entity: "StationEntity") else { return }
+    cache.saveStationsToCache(stations: stations)
   }
 }
